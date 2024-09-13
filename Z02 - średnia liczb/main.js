75e47c1605824f5d4f53ecb722e184a78c6b0c5f
BombiuszWielki <164795181+BombiuszWielki@users.noreply.github.com>
2024-09-13 15:16:58 +0200

    Add files via upload

---
+++ Z02 - średnia liczb/main.js
@@ -0,0 +1,11 @@
+const liczba_a = document.querySelector('#a');
+const liczba_b = document.querySelector('#b');
+const liczba_c = document.querySelector('#c');
+const liczba_d = document.querySelector('#d');
+
+const wynik = document.querySelector('#wynik');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function (){
+    
+});
